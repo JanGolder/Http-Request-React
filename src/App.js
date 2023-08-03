c7c3b8dd133149027ad20684671b77c9fd636951
Jan Golder <kontakt.jangolder@gmail.com>
2023-08-03 19:29:15 +0200

    add auto fetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -27,7 +27,7 @@ function App() {
   //     });
   // }
 
-  async function fetchMoviesHandler() {
+const fetchMoviesHandler = useCallback(async()=> {
     setIsLoading(true);
     setError(null);
     try {
@@ -52,7 +52,11 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  },[]);
+
+  useEffect(()=>{
+    fetchMoviesHandler();
+  },[fetchMoviesHandler]);
 
   return (
     <React.Fragment>
